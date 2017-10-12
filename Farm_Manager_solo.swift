668736d5f46b18131bb7bdf527d7df6dc5b22b72
MadNoodle <mjanneau@gmail.com>
2017-10-12 11:54:13 +0200

    ajout d'un switch pour accceder aux choix

--- Farm_Manager_solo.swift
+++ Farm_Manager_solo.swift
@@ -3,3 +3,17 @@ print("Que voulez-vous faire?"
     + "\n1. Enregistrez une nouvelle activité"
     + "\n2. Connsultez ma banque"
     + "\n3. Consultez ma grange")
+
+if let choice = readLine(){ // Renvoie un optionnel donc j unwrap
+    switch(choice){
+        case "1":
+            print("Vous voulez ajouter une activité")
+        case "2":
+            print("Vous voulez consulter votre banque")
+        case "3":
+            print("Vous voulez consulter votre grange")
+        default:
+            print("je ne comprends pas")
+    }
+    
+}
