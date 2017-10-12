ee7ab6a9ec2295e3863d49e9b9f79cf524a5264e
MadNoodle <mjanneau@gmail.com>
2017-10-12 12:06:00 +0200

    ajout de fonctions pour banque et grange

--- Farm_Manager_solo.swift
+++ Farm_Manager_solo.swift
@@ -1,7 +1,32 @@
+// /////////////// //
+// MARK: VARIABLES //
+// /////////////// //
+var money = 0.0 // Double car j'ai des centimes
+var barn = ["Milk":0, "Wheat":0, "Wool" : 0] //Déclaration du dictionnaire grange
+
+// /////////////// //
+// MARK: ACTIVITIES //
+// /////////////// //
+
+func displayBank (){
+    print("Vous avez \(money) € sur votre compte en banque")
+}
+
+func displayBarn(){
+    print("Voici le contenu de la grange:"
+        + "\n \(barn["Milk"]!) bidons de lait" //Force unwrap car c'est optionnel
+        + "\n \(barn["Wheat"]!) bottes de blé"
+        + "\n \(barn["Wool"]!) pelotes de laine")
+}
+
+// ////////////////// //
+// MARK: INTERACTIONS //
+// ////////////////// //
+
 //Prompt options 1st level
 print("Que voulez-vous faire?"
     + "\n1. Enregistrez une nouvelle activité"
-    + "\n2. Connsultez ma banque"
+    + "\n2. Consultez ma banque"
     + "\n3. Consultez ma grange")
 
 if let choice = readLine(){ // Renvoie un optionnel donc j unwrap
@@ -9,9 +34,9 @@ if let choice = readLine(){ // Renvoie un optionnel donc j unwrap
         case "1":
             print("Vous voulez ajouter une activité")
         case "2":
-            print("Vous voulez consulter votre banque")
+            displayBank()
         case "3":
-            print("Vous voulez consulter votre grange")
+            displayBarn()
         default:
             print("je ne comprends pas")
     }
